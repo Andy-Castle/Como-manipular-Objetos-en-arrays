1cccc0bc7c69cf684320da51aaadf6ea61d8d08a
Andy-Castle <masterchief_and@live.com>
2024-09-29 22:30:05 -0500

    ok, esto ya esta mañana le sigo

---
+++ ProjectTodo.js
@@ -0,0 +1,62 @@
+let ProjecstTodos = [
+  {
+    id: 1,
+    title: "Default",
+    todoList: [
+      {
+        id: 1,
+        title: "Demo",
+        dueDate: "30/09/2024",
+        priority: "Low",
+        status: "Doing",
+      },
+      {
+        id: 2,
+        title: "Tutorial",
+        dueDate: "2/10/2024",
+        priority: "Medium",
+        status: "Doing",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Desarrollo Movil",
+    todoList: [
+      {
+        id: 1,
+        title: "Mockups",
+        dueDate: "2/10/2024",
+        priority: "Medium",
+        status: "Doing",
+      },
+      {
+        id: 2,
+        title: "Data Base",
+        dueDate: "3/10/2024",
+        priority: "Medium",
+        status: "Doing",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "PWA",
+    todoList: [
+      {
+        id: 1,
+        title: "Programming",
+        dueDate: "5/10/2024",
+        priority: "Low",
+        status: "Doing",
+      },
+      {
+        id: 2,
+        title: "Testing",
+        dueDate: "7/10/2024",
+        priority: "Medium",
+        status: "Doing",
+      },
+    ],
+  },
+];
--- index.js
+++ index.js
@@ -17,8 +17,15 @@ let ObjArr = [
     age: 27,
     interest: ["Soccer player", "Good driver"],
   },
+  {
+    id: 4,
+    name: "Pedro",
+    age: 28,
+    interest: ["Soccer player", "Good driver"],
+  },
 ];
 
+//Esta función sirve para encontrar un Obj por su id
 function findObjById(id) {
   const result = ObjArr.filter((obj) => obj.id === id);
   return result;
@@ -26,6 +33,7 @@ function findObjById(id) {
 
 let resultObj = findObjById(1);
 
+//Esta función sirve para eliminar un(1) interes
 function dropOneInterest(id, dropOneInterest) {
   let array = null;
   let resultObj = null;
@@ -50,6 +58,7 @@ function dropOneInterest(id, dropOneInterest) {
 
 let dropInterest = dropOneInterest(1, "Singer");
 
+//Esta función sirve para eliminar un objeto dentro de un array
 function dropOneObj(id) {
   let result = ObjArr.filter((obj) => obj.id !== id);
   ObjArr = result;
@@ -58,6 +67,7 @@ function dropOneObj(id) {
 
 let dropOneObject = dropOneObj(3);
 
+//Esta función sirve para cambiar el nombre y edad de un objeto
 function changeNameAge(id, name = null, age = null) {
   let result = ObjArr.filter((obj) => obj.id === id);
   for (const elements of result) {
@@ -75,6 +85,7 @@ function changeNameAge(id, name = null, age = null) {
 
 let changing = changeNameAge(2, "Daniel", 30);
 
+//Esta función sirve para modificar solo un(1) interes
 function changeInterest(id, oldInterest, newInterest) {
   let result = ObjArr.filter((obj) => obj.id === id);
   result.forEach((e) => {
@@ -87,5 +98,31 @@ function changeInterest(id, oldInterest, newInterest) {
 }
 
 let changeInt = changeInterest(2, "Singing", "Playing video Games");
+
+let newInterest = ["English C1", "Painting", "Singer"];
+
+//Esta función sirve para cambiar todo el interes
+function changeInterestPart(id, newInterest) {
+  let result = ObjArr.filter((obj) => obj.id === id);
+  result.forEach((e) => {
+    e.interest = newInterest;
+  });
+
+  result = ObjArr;
+  return result;
+}
+
+let CIP = changeInterestPart(1, newInterest);
+
+//Esta función sirve para crear un nuevvo objeto y añadirlo
+function addNewObj(name, age, interest) {
+  let lastIndex = ObjArr.length - 1;
+  let lastObj = ObjArr[lastIndex].id;
+  let idLastObj = lastObj;
+  ObjArr.push({ id: idLastObj + 1, name: name, age: age, interest: interest });
+  return ObjArr;
+}
+
+addNewObj("Paola", 21, ["Gaming", "Singing", "Playing soccer"]);
 
 console.log(ObjArr);
